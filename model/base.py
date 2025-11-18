7e7a885248071666bc5df49796df913890eb355a
Tancrede <fantomasetgatsby@gmail.com>
2025-11-18 15:03:05 +0100

    Old parameters removal

--- model/base.py
+++ model/base.py
@@ -8,16 +8,14 @@ for each model.
 
 
 class PINN(nn.Module):
-    def __init__(self,conf):
+    def __init__(self,config):
         super(PINN, self).__init__()
-        self.input_dim = conf['input_dim']
-        self.hidden_dim = conf['hidden_dim']
-        self.output_dim = conf['output_dim']
-        self.num_layers = conf['num_layers']
-        self.first_omega_0 = conf['first_omega']
-        self.hidden_omega_0 = conf['hidden_omega']
-        self.model_type = conf['model']
-        self.device = conf['device']
-        self.sigma = conf['sigma']
-        self.mu = conf['mu']
-        self.sigma_fourier = conf['sigma_fourier']
+        self.input_dim = config['input_dim']
+        self.hidden_dim = config['hidden_dim']
+        self.output_dim = config['output_dim']
+        self.num_layers = config['num_layers']
+        self.first_omega_0 = config['first_omega']
+        self.hidden_omega_0 = config['hidden_omega']
+        self.model_type = config['model']
+        self.device = config['device']
+        self.sigma_fourier = config['sigma_fourier']
